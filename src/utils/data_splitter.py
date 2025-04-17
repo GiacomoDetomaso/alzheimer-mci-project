c5de447a19e5c434ed8836049146f2705bbff6d3
Giacomo Detomaso <57029496+GiacomoDetomaso@users.noreply.github.com>
2025-04-17 12:21:10 +0200

    Random undersampled majority class

--- src/utils/data_splitter.py
+++ src/utils/data_splitter.py
@@ -1,13 +1,23 @@
+from random import Random
 import pandas as pd
+import random
 
 from sklearn.model_selection import train_test_split
-from imblearn.under_sampling import RandomUnderSampler
 
 class DataSplitterTrainTest:
     def __init__(self, subject_experiments:pd.Series, diagnosis:pd.Series):
         self.subject_experiments = subject_experiments
         self.diagnosis = diagnosis
-
+        
+        # Basic splitting
+        #self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+        #    subject_experiments, 
+        #    diagnosis, 
+        #    stratify=diagnosis,
+        #    random_state=42,
+        #    test_size=0.25
+        #)
+    
     def set_transformations(self, transformations_se:pd.Series, transformations_d:pd.Series):
         self.subject_experiments = pd.concat([self.subject_experiments, transformations_se], ignore_index=True)
         self.diagnosis = pd.concat([self.diagnosis, transformations_d], ignore_index=True)
@@ -15,18 +25,14 @@ class DataSplitterTrainTest:
     def __move_files(self):
         pass
 
-    def __rand_undersample_majority_class(self):
+    def __check_subjects_distribution(X_train, X_test, ):
+        pass
+    
+    def get_basic_train_set():
         pass
 
-    def split(self):
-        # TODO exclude lables
-        X_train, X_test, y_train, y_test = train_test_split(
-            self.subject_experiments, 
-            self.diagnosis, 
-            stratify=self.diagnosis,
-            random_state=42,
-            test_size=0.25
-        )
-
-        return X_train, X_test, y_train, y_test
-
+    def get_test_set(self):
+        for t in self.X_train:
+            subject = t.split('_')[0]
+            
+            return         
--- src/utils/dataset_builder.py
+++ src/utils/dataset_builder.py
@@ -1,12 +1,46 @@
-from glob import glob
+from genericpath import isdir
 import os
+import pandas as pd
+import random
+import shutil
 
-class MonaiDataSetBuilder:
-    def __init__(self, scans_dir_path:str, labels:list):
-        self.scans_dir_path = scans_dir_path
-        self.labels = labels
+from glob import glob
 
-    def build(self):
-        image_files = glob(self.scans_dir_path)
+class DatasetHelper:
 
-        return [{'image': img, 'label': label} for img, label in zip(self.scans_dir_path, self.labels)]
+    @staticmethod
+    def undersample_majority_class(subject_experiments:pd.Series, diagnosis:pd.Series, final_size:int):        
+        # TODO raise error if class is not series
+        
+        # Get majority class
+        majority_class = diagnosis.value_counts().idxmax()   
+        
+        # Consider only majority class instances
+        majority_labels = diagnosis[diagnosis == majority_class]
+        other_labels = diagnosis[diagnosis != majority_class]
+        resampled = majority_labels.sample(n=final_size, random_state=42)
+        
+        # Build the new dataset with the majority class resampled
+        new_diagnosis = pd.concat([resampled, other_labels]).sort_values()
+        
+        return new_diagnosis.index
+    
+    @staticmethod
+    def move_folders(subject_experiments:list, base_dir:str, dst_dir:str):
+        if not os.path.isdir(dst_dir):
+            os.mkdir(dst_dir)
+            
+        for subject in subject_experiments:
+            src_dir = os.path.join(base_dir, subject)
+            dst_copy = os.path.join(dst_dir, subject)
+            
+            shutil.copytree(src_dir, dst_copy)
+        
+    @staticmethod
+    def get_dataset(images:str, labels:list):
+        image_files = sorted(glob(images))
+
+        return [
+            {'image': image, 'label': label}
+            for image, label in zip(image_files, labels)
+        ]
